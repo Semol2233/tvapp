9d5a75ee9294b4920df2d751b6607c395d6b21e1
Semol <semolmahamud2233@gmail.com>
2021-10-15 00:09:32 +0600

    fhtj

--- Data_app/views.py
+++ Data_app/views.py
@@ -360,7 +360,6 @@ from django.contrib.postgres.search import SearchQuery, SearchVector
 
 
 
-
 class qa_pagenation(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
@@ -371,7 +370,7 @@ class seeearcsssh_filter(APIView, PaginationHandlerMixin):
 
     def get(self,request,query):
         result = []
-        filter_postmodel_q = Add_Channel.objects.annotate(search=SearchVector('id','channel_name','straming_url','channel_logo','catgory__cat_name'),).filter(search=query).values('id','channel_name','catgory__cat_name','channel_logo')
+        filter_postmodel_q = Add_Channel.objects.filter(Q(channel_name__contains=query) | Q(catgory__cat_name__contains=query)).values('id','channel_name','catgory__cat_name','channel_logo')
         if filter_postmodel_q:
             for p in filter_postmodel_q:
                 result.append(p)
